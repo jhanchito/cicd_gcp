6c728e9457f131ee1098d33a3a91ddcbcdf81f89
Jhampier <jhampiertapia@gmail.com>
2025-06-17 00:20:30 +0000

    tercer commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def main():
-    model = {"title": "Hello Build Trigger."}
+    model = {"title": "Hello Build Trigger. JHANCHITO"}
     return render_template("index.html", model=model)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
